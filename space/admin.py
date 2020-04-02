64199aa5d1d3e183358fd28e8a9a3ad7ca955647
cristhian <cristhian9bravo@gmail.com>
2020-04-02 07:42:31 -0500

    (tables) cchore: clean admin.py file in space app

--- space/admin.py
+++ space/admin.py
@@ -1,14 +1,1 @@
 from django.contrib import admin
-from .models import Country
-
-
-@admin.register(Country)
-class CountryAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "code",
-        "language",
-        "extra",
-        "currency"
-    )
-    raw_id_fields = ("currency",)
